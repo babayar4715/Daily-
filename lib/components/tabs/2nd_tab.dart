abf7448dddbcf786f93a382fc07e04b71ff84291
babayar4715 <babayarkenzhebekov@gmail.com>
2023-03-30 18:58:30 +0300

    pull+

--- lib/components/tabs/2nd_tab.dart
+++ lib/components/tabs/2nd_tab.dart
@@ -1,19 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class SecondTab extends StatelessWidget {
+class SecondTab extends StatefulWidget {
   const SecondTab({Key? key}) : super(key: key);
+
+  @override
+  State<SecondTab> createState() => _SecondTabState();
+}
+
+class _SecondTabState extends State<SecondTab> {
+  DateTime today = DateTime.now();
+
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      body: Row(
-        children: const [
-          Text(
-            "there are we have to put callendar and list",
-          )
-        ],
-      ),
+      body: content(),
+    );
+  }
+
+  Widget content() {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Container(
+            child: TableCalendar(
+              locale: "en_US",
+              rowHeight: 50,
+              headerStyle: const HeaderStyle(
+                formatButtonVisible: false,
+                titleCentered: true,
+              ),
+              availableGestures: AvailableGestures.all,
+              selectedDayPredicate: (day) => isSameDay(day, today),
+              focusedDay: today,
+              firstDay: DateTime.utc(2010, 10, 15),
+              lastDay: DateTime.utc(2030, 1, 15),
+              onDaySelected: _onDaySelected,
+            ),
+          ),
+        ),
+        Text("Selected Day = " + today.toString().split("")[0]),
+      ],
     );
   }
 }
